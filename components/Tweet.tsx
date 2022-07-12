f6a35a113b58d2b4bceb5c00b85a88989a7d5776
Jason Pallone <50902289+Jason-Pallone@users.noreply.github.com>
2022-07-12 16:43:20 -0400

    Adding tweet interaction icons

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Tweet } from "../typings";
 import TimeAgo from 'react-timeago'
 import { TwitterTweetEmbed } from "react-twitter-embed";
+import { ChatAlt2Icon, HeartIcon, SwitchHorizontalIcon, UploadIcon } from "@heroicons/react/outline";
 
 interface Props {
   tweet: Tweet
@@ -34,6 +35,24 @@ const Tweet = ({ tweet }: Props) => {
                className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
              />            
             }
+          </div>
+        </div>
+
+        <div className="mt-5 flex justify-between">
+          <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+            <ChatAlt2Icon className="h5 w-5" />
+          </div> 
+
+          <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+            <SwitchHorizontalIcon className="h-5 w-5" />
+          </div>
+
+          <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+            <HeartIcon className="h-5 w-5" />
+          </div>
+
+          <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+            <UploadIcon className="h-5 w-5" />
           </div>
         </div>
       </div>
